3faee9e4b0a5e10c9124f325f1e0ac15711e2bc3
Alaa Abu El-wafa <lolaalaa194@gmail.com>
2026-05-07 21:52:17 +0300

    add TYPE_PRIVATE

--- protocol.h
+++ protocol.h
@@ -1,16 +1,19 @@
 #ifndef PROTOCOL_H
 #define PROTOCOL_H
 
-#define MAX_BUF 4096
-#define TYPE_CHAT 1
-#define TYPE_FILE 2
-#define TYPE_LIST 3 
+#define MAX_BUF 8192
+#define MAX_NAME 50
+
+#define TYPE_CHAT    1
+#define TYPE_FILE    2
+#define TYPE_LIST    3 
+#define TYPE_PRIVATE 4
 
 typedef struct {
     int type;
     int payload_size;
-    char filename[64];
-    char sender_name[50];
+    char filename[128];
+    char sender_name[MAX_NAME];
 } PacketHeader;
 
 #endif
